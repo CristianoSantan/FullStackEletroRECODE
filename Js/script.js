42f5083b1f9b60da74fc25d94d7502bf1913f1ab
cristianosouzasantana <cristiano.mer@gmail.com>
2020-10-16 02:21:42 -0300

    first commit

---
+++ Js/script.js
@@ -0,0 +1,31 @@
+//  -----------------------------------   Exibição por categoria 
+function exibirCategoria(categoria) {
+    let elementos = document.getElementsByClassName('card');
+    console.log(elementos);
+    for(var i=0; i<elementos.length; i++){
+        console.log(elementos[i].id);
+        if(categoria == elementos[i].id){
+            elementos[i].style = "";
+        }
+        else {
+            elementos[i].style = "display:none";
+        }  
+    }
+}
+//  -----------------------------------   Exibir todos
+let exibirTodos = () => {
+    let elementos = document.getElementsByClassName('card');
+
+    for(var i=0; i<elementos.length; i++){
+            elementos[i].style = ""; 
+    }
+}
+//  -----------------------------------   Destacar imagem   
+let destaque = (imagem) => {
+    if(imagem.height == 160) {
+        imagem.height = 120;
+    }
+    else {
+        imagem.height = 160;
+    }
+}
